4fb39eb93e8828170d5374d5869c59b829a9a677
Mohammed Damerdji <od.med.614@gmail.com>
2023-03-24 21:27:01 +0100

    v1 done

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function change_theme() {
         document.getElementById("div2").style.backgroundColor = "hsl(235, 24%, 19%)";
         document.getElementById("div2").style.color = "white";
         document.getElementById("theme_icon").src = "icon-moon.svg"
+        document.getElementById("todo0").style.color = "white";
         dark = true;
     } else {
         document.getElementById("h1").style.backgroundImage = "url('bg-desktop-light.jpg')";
@@ -18,6 +19,7 @@ function change_theme() {
         document.getElementById("div2").style.backgroundColor = "white";
         document.getElementById("div2").style.color = "black";
         document.getElementById("theme_icon").src = "icon-sun.svg";
+        document.getElementById("todo0").style.color = "black";
         dark = false;
     }
 }
