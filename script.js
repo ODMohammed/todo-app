e1ccd044489985de3162625b3afe63e1071b087c
Mohammed Damerdji <od.med.614@gmail.com>
2023-03-28 00:47:03 +0100

    adding delete button

--- script.js
+++ script.js
@@ -44,10 +44,34 @@ document.addEventListener("keyup", function(e) {
 
 function addtodo(todo) {
     const ul = document.getElementById("p_list");
+    const tb = document.createElement("table");
+    const tr = document.createElement("tr");
+    const thl = document.createElement("th");
+    const thr = document.createElement("th");
     const li = document.createElement("li");
     const hr = document.createElement("hr");
+    const deleteBtn = document.createElement('button');
+    const dlImg = document.createElement("img");
+
+    tb.style.width = "100%";
+    thl.style.width = "90%";
+    thr.style.width = "10%";
+    thr.style.textAlign = "right";
+    dlImg.src = "icon-cross.svg";
     hr.classList.add("hr_class");
-    li.innerText = todo;
+    thl.innerText = todo;
+
+    thr.appendChild(deleteBtn);
+    deleteBtn.appendChild(dlImg);
+    tr.appendChild(thl);
+    tr.appendChild(thr);
+    tb.appendChild(tr);
+    li.appendChild(tb);
     ul.appendChild(li);
     ul.appendChild(hr);
+
+    deleteBtn.addEventListener('click', () => {
+        li.remove();
+        hr.remove();
+    });
 }
